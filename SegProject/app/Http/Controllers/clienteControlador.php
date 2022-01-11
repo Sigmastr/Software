327fda9ebadaf86384c232d844efc06791d0c6ab
Sigmastr <cristianmonttfierro@gmail.com>
2022-01-11 18:12:02 -0300

    Implementción laravel

---
+++ SegProject/app/Http/Controllers/clienteControlador.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+class clienteControlador extends Controller{
+
+    public function index()
+    {
+        //$clientes= DB::select('select nombre,rut,email,telefono,actividad,representante from cliente');
+
+    }
+}
